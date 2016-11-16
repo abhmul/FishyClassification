f2fd5510bdbd468698e48661c059bb219da02f6b
abhmul <abhmul@gmail.com>
2016-11-16 15:47:14 -0600

    Not using zoom or shear

--- src/train.py
+++ src/train.py
@@ -38,8 +38,8 @@ def run_cross_validation_create_models(nfolds=10, model_func=vgg_model):
             featurewise_std_normalization=True,
             # width_shift_range=0.2,
             # height_shift_range=0.2,
-            shear_range=0.2,
-            zoom_range=0.2,
+            # shear_range=0.2,
+            # zoom_range=0.2,
             horizontal_flip=True,
             # vertical_flip=True,
             fill_mode='nearest')

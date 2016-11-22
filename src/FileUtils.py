3cb3047b42da81266fe3daddeb469e29e05d1c2f
tian000 <tian543@gmail.com>
2016-11-21 19:44:05 -0600

    image scripts

---
+++ src/FileUtils.py
@@ -0,0 +1,31 @@
+import os
+
+def get_all_label_folders():
+    train_folder_path = "../input/train";
+    label_folders = []
+    
+    for label_folder in os.listdir(train_folder_path):
+        label_folder_path = train_folder_path + "/" + label_folder
+        label_folders.append(label_folder_path)
+    
+    return label_folders
+
+
+def get_all_cropped_folders():
+    label_folders = get_all_cropped_folders()
+    cropped_folders = []
+    
+    for label_folder_path in label_folders:
+        for cropped_folder in os.listdir(label_folder_path):
+            if cropped_folder.lower() == "cropped":
+                cropped_folders.append(label_folder_path + "/" + cropped_folder)
+    
+    return cropped_folders
+
+def get_all_no_fish_files():
+    label_folders = get_all_label_folders()
+    no_fish_folder_path = [l for l in label_folders if l.lower().endswith("nof")][0]
+    no_fish_files = [no_fish_folder_path + "/" + f for f in os.listdir(no_fish_folder_path)]
+    return no_fish_files
+    
+print(get_all_no_fish_files())
---
+++ src/cropped_img_heatmap.py
@@ -0,0 +1,37 @@
+import numpy as np
+import numpy.random
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+
+# the cropped img dimensions
+x = []
+y = []
+
+# Load data
+train_folder_path = "../input/train";
+cropped_folders = []
+
+for label_folder in os.listdir(train_folder_path):
+    label_folder_path = train_folder_path + "/" + label_folder
+    for cropped_folder in os.listdir(label_folder_path):
+        if cropped_folder.lower() == "cropped":
+            cropped_folders.append(label_folder_path + "/" + cropped_folder)
+
+for cropped_folder in cropped_folders:
+    for cropped_file in os.listdir(cropped_folder):
+        cropped_file_path = cropped_folder + "/" + cropped_file
+        im = Image.open(cropped_file_path)
+        width, height = im.size
+        x.append(width)
+        y.append(height)
+
+x = np.array(x)
+y = np.array(y)
+
+heatmap, xedges, yedges = np.histogram2d(x, y, bins=25)
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+plt.clf()
+plt.imshow(heatmap.T, extent=extent, origin='lower')
+plt.show()

1548e042edbca1738b9d821556803f216ecd5415
abhmul <abhmul@gmail.com>
2016-12-18 21:05:24 -0600

    Rewrote Kfold

---
+++ src/kfold_utils.py
@@ -0,0 +1,146 @@
+from __future__  import print_function
+
+import shutil
+import os
+
+import numpy as np
+
+from keras.callbacks import ModelCheckpoint
+
+
+class KFoldFromDir(object):
+
+    def __init__(self, nfolds, class_labels,
+                 root='../input',
+                 total_data='train',
+                 train_data='train_split',
+                 val_data='val_split'):
+
+        self.nfolds = nfolds
+        self.root = root
+        self.train_data = train_data
+        self.val_data = val_data
+        self.total_data_dir = os.path.join(self.root, total_data)
+        self.train_data_dir = os.path.join(self.root, self.train_data)
+        self.val_data_dir = os.path.join(self.root, self.val_data)
+        self.class_labels = class_labels
+
+    @staticmethod
+    def copy(img, class_label, total_dir, split_dir):
+        source = os.path.join(total_dir, class_label, img)
+        target = os.path.join(split_dir, class_label, img)
+        try:
+            shutil.copy(source, target)
+            return True
+        except IOError, e:
+            print("Unable to copy file. %s" % e)
+            return False
+
+    @staticmethod
+    def copy_folder(images, class_label, total_dir, split_dir, counter=0):
+
+        for img in images:
+            success = KFoldFromDir.copy(img, class_label, total_dir, split_dir)
+            if success:
+                counter += 1
+        return counter
+
+    @staticmethod
+    def make_split_dir(class_label, image_split, total_dir, split_dir, split_type='', counter=0):
+
+        split_type += ' '
+        print('Rebuilding {}directory tree...'.format(split_type))
+        if class_label not in os.listdir(split_dir):
+            os.mkdir(os.path.join(split_dir, class_label))
+
+        print('Copying {}split to folder {}...'.format(split_type, class_label))
+        counter = KFoldFromDir.copy_folder(image_split, class_label, total_dir, split_dir, counter=counter)
+
+        return counter
+
+    def remove_past_splits(self):
+
+        # Remove the train and val dirs if they already exist and create empty versions
+        data_dirs = set(os.listdir(self.root))
+        if self.train_data in data_dirs:
+            shutil.rmtree(self.train_data_dir)
+        if self.val_data in data_dirs:
+            shutil.rmtree(self.val_data_dir)
+        os.mkdir(self.train_data_dir)
+        os.mkdir(self.val_data_dir)
+
+    def fit(self, model_func, train_datagen, val_datagen, model_name, nbr_epochs=25,
+            img_width=299, img_height=299, batch_size=32, callbacks=[]):
+
+        for i in range(self.nfolds):
+            # Clean the current split
+            print('Removing past splits...')
+            self.remove_past_splits()
+
+            #  Initialize the the enumerators for train and val data
+            nbr_train_samples = 0
+            nbr_validation_samples = 0
+
+            for class_label in self.class_labels:
+
+                # List out all of the images of species fish
+                print('Performing split {} for species {}'.format(i, class_label))
+                total_images = np.array(os.listdir(os.path.join(self.total_data_dir, class_label)))
+                inds = np.arange(len(total_images))
+                # Shuffle them if this is our first fold
+                np.random.shuffle(inds) if not i else None
+                # get the validation start and end ind
+                split_ind_begin = int(1. / self.nfolds * i * len(inds))
+                split_ind_end = int(1. / self.nfolds * (i + 1) * len(inds))
+
+                # Make the train images by making an array without the fold indices
+                print('Making train split...')
+                train_images = total_images[np.concatenate((inds[:split_ind_begin], inds[split_ind_end:]))]
+                nbr_train_samples += self.make_split_dir(class_label,
+                                                         train_images,
+                                                         self.total_data_dir,
+                                                         self.train_data_dir,
+                                                         split_type='train')
+
+                # Make the validation images by making an array with the fold indicies
+                print('Making validation split...')
+                val_images = total_images[inds[split_ind_begin:split_ind_end]]
+                nbr_validation_samples += self.make_split_dir(class_label,
+                                                              val_images,
+                                                              self.total_data_dir,
+                                                              self.val_data_dir,
+                                                              split_type='validation')
+
+            train_generator = train_datagen.flow_from_directory(
+                self.train_data_dir,
+                target_size=(img_width, img_height),
+                batch_size=batch_size,
+                shuffle=True,
+                classes=self.class_labels,
+                class_mode='categorical')
+
+            validation_generator = val_datagen.flow_from_directory(
+                self.val_data_dir,
+                target_size=(img_width, img_height),
+                batch_size=batch_size,
+                shuffle=True,
+                classes=self.class_labels,
+                class_mode='categorical')
+
+            # autosave best Model
+            best_model_file = model_name + '_fold{}.h5'
+            best_model = ModelCheckpoint(best_model_file, monitor='val_loss', verbose=1, save_best_only=True)
+
+            model = model_func()
+            print('Training Model...')
+            model.fit_generator(
+                train_generator,
+                samples_per_epoch=nbr_train_samples,
+                nb_epoch=nbr_epochs,
+                validation_data=validation_generator,
+                nb_val_samples=nbr_validation_samples,
+                verbose=1,
+                callbacks=[best_model] + callbacks)
+
+
+
--- src/train.py
+++ src/train.py
@@ -10,13 +10,18 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.layers import Flatten, Dense, MaxPooling2D, AveragePooling2D
 from keras.models import Model
 from keras.optimizers import RMSprop, SGD
-from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
+
+from kfold_utils import KFoldFromDir
 
 random.seed(2016)
 np.random.seed(2016)
 
-logging.basicConfig(filename='example.log', level=logging.DEBUG)
+root = '../input'
+total_data = 'train'
+train_data = 'train_split'
+val_data = 'val_split'
+
 
 learning_rate = 0.0001
 img_width = 299
@@ -24,38 +29,32 @@ img_height = 299
 nbr_epochs = 25
 batch_size = 32
 nfolds = 10
-
-root = '../input'
-train_total = 'train'
-train_data = 'train_split'
-val_data = 'val_split'
-
-train_total_dir = os.path.join(root, train_total)
-train_data_dir = os.path.join(root, train_data)
-val_data_dir = os.path.join(root, val_data)
-
 FishNames = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
 
-print('Loading InceptionV3 Weights ...')
-InceptionV3_notop = InceptionV3(include_top=False, weights='imagenet',
-                                input_tensor=None, input_shape=(299, 299, 3))
-# Note that the preprocessing of InceptionV3 is:
-# (x / 255 - 0.5) x 2
+
+def inception_model():
+
+    print('Loading InceptionV3 Weights ...')
+    InceptionV3_notop = InceptionV3(include_top=False, weights='imagenet',
+                                    input_tensor=None, input_shape=(299, 299, 3))
+    # Note that the preprocessing of InceptionV3 is:
+    # (x / 255 - 0.5) x 2
+
+    print('Adding Average Pooling Layer and Softmax Output Layer ...')
+    output = InceptionV3_notop.get_layer(index=-1).output  # Shape: (8, 8, 2048)
+    output = AveragePooling2D((8, 8), strides=(8, 8), name='avg_pool')(output)
+    output = Flatten(name='flatten')(output)
+    output = Dense(8, activation='softmax', name='predictions')(output)
+
+    InceptionV3_model = Model(InceptionV3_notop.input, output)
+    # InceptionV3_model.summary()
 
 
-print('Adding Average Pooling Layer and Softmax Output Layer ...')
-output = InceptionV3_notop.get_layer(index=-1).output  # Shape: (8, 8, 2048)
-output = AveragePooling2D((8, 8), strides=(8, 8), name='avg_pool')(output)
-output = Flatten(name='flatten')(output)
-output = Dense(8, activation='softmax', name='predictions')(output)
+    print('Creating optimizer and compiling')
+    optimizer = SGD(lr=learning_rate, momentum=0.9, decay=0.0, nesterov=True)
+    InceptionV3_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-InceptionV3_model = Model(InceptionV3_notop.input, output)
-# InceptionV3_model.summary()
-
-
-print('Creating optimizer and compiling')
-optimizer = SGD(lr=learning_rate, momentum=0.9, decay=0.0, nesterov=True)
-InceptionV3_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return InceptionV3_model
 
 print('Initializing Augmenters')
 # this is the augmentation configuration we will use for training
@@ -68,113 +67,13 @@ train_datagen = ImageDataGenerator(
     height_shift_range=0.1,
     horizontal_flip=True)
 
-print('Running {} folds'.format(nfolds))
-for i in xrange(nfolds):
-    # Clean the current split
-    print('Removing past splits...')
-    # Remove the train and val dirs if they already exist and create empty versions
-    data_dirs = set(os.listdir(root))
-    if train_data in data_dirs:
-        shutil.rmtree(train_data_dir)
-    if val_data in data_dirs:
-        shutil.rmtree(val_data_dir)
-    os.mkdir(train_data_dir)
-    os.mkdir(val_data_dir)
+# this is the augmentation configuration we will use for validation:
+# only rescaling
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    # these are the names of the species in alphabetical order
-    FishNames = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
-
-    # Initialize the the enumerators for train and val data
-    nbr_train_samples = 0
-    nbr_validation_samples = 0
-
-    # make a split
-    for fish in FishNames:
-        # List out all of the images of species fish
-        print('Performing split {} for species {}'.format(i, fish))
-        total_images = np.array(os.listdir(os.path.join(train_total_dir, fish)))
-        # Shuffle them if this is our first fold
-        np.random.shuffle(total_images) if not i else None
-        # get the validation start and end ind
-        split_ind_begin = int(1. / nfolds * i * len(total_images))
-        split_ind_end = int(1. / nfolds * (i+1) * len(total_images))
-
-        # Make the directory for the species if it is not there yet (Train)
-        print('Rebuilding train directory tree...')
-        if fish not in os.listdir(train_data_dir):
-            os.mkdir(os.path.join(train_data_dir, fish))
-
-        # Make the train images by making an array without the fold indices
-        print('Making train split...')
-        train_images = np.concatenate((total_images[:split_ind_begin], total_images[split_ind_end:]))
-
-        # copy each each image in the train set
-        print('Copying train split to folder {}...'.format(fish))
-        for img in train_images:
-            source = os.path.join(train_total_dir, fish, img)
-            target = os.path.join(train_data_dir, fish, img)
-            shutil.copy(source, target)
-            nbr_train_samples += 1
-
-        # Repeat the process for the validation data (Wrap this process into a single function)
-        # Make the directory for the species if it is not there yet (Validation)
-        print('Rebuilding validation directory tree...')
-        if fish not in os.listdir(val_data_dir):
-            os.mkdir(os.path.join(val_data_dir, fish))
-
-        # Slice out the validation images
-        print('Making validation split...')
-        val_images = total_images[split_ind_begin:split_ind_end]
-
-        # copy each each image in the val set
-        print('Copying validation split to folder {}...'.format(fish))
-        for img in val_images:
-            source = os.path.join(train_total_dir, fish, img)
-            target = os.path.join(val_data_dir, fish, img)
-            shutil.copy(source, target)
-            nbr_validation_samples += 1
-
-    print('All done performing split for fold {}'.format(i))
-
-    print('Initializing Checkpointer...')
-    # autosave best Model
-    best_model_file = "../inception_weights_fold{}.h5".format(i)
-    best_model = ModelCheckpoint(best_model_file, monitor='val_acc', verbose=1, save_best_only=True)
-
-    print('Creating Generators...')
-    # this is the augmentation configuration we will use for validation:
-    # only rescaling
-    val_datagen = ImageDataGenerator(rescale=1. / 255)
-
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        shuffle=True,
-        # save_to_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/visualization',
-        # save_prefix = 'aug',
-        classes=FishNames,
-        class_mode='categorical')
-
-    validation_generator = val_datagen.flow_from_directory(
-        val_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        shuffle=True,
-        # save_to_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/visulization',
-        # save_prefix = 'aug',
-        classes=FishNames,
-        class_mode='categorical')
-
-    print('Training Model...')
-    try:
-        InceptionV3_model.fit_generator(
-            train_generator,
-            samples_per_epoch=nbr_train_samples,
-            nb_epoch=nbr_epochs,
-            validation_data=validation_generator,
-            nb_val_samples=nbr_validation_samples,
-            verbose=1,
-            callbacks=[best_model])
-    except Exception as e:  # most generic exception you can catch
-        logging.exception("message")
+kf = KFoldFromDir(nfolds, FishNames, root=root, total_data=total_data, train_data=train_data, val_data=val_data)
+kf.fit(inception_model, train_datagen, val_datagen, '../fishyInception',
+       nbr_epochs=nbr_epochs,
+       img_width=img_width,
+       img_height=img_height,
+       batch_size=batch_size)

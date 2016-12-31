74c66952b6ccdc14cec4a6c6f578366cafe4e679
abhmul <abhmul@gmail.com>
2016-12-31 15:42:06 -0500

    bugfixed model constructor

--- src/models.py
+++ src/models.py
@@ -5,10 +5,13 @@ from keras.optimizers import SGD
 import keras.backend as K
 
 
-def inception_model(input_shape=(256, 256, 3), fcn=True, test=False, learning_rate=0.0001, dim_ordering='default'):
+def inception_model(input_shape=None, fcn=True, test=False, learning_rate=0.0001, dim_ordering='default'):
 
     if dim_ordering == 'default':
         dim_ordering = K.image_dim_ordering()
+
+    if input_shape is None:
+        input_shape = (256, 256, 3) if dim_ordering == 'tf' else (3, 256, 256)
 
     print('Loading InceptionV3 Weights ...')
     InceptionV3_notop = InceptionV3(include_top=False, weights='imagenet',
@@ -17,7 +20,7 @@ def inception_model(input_shape=(256, 256, 3), fcn=True, test=False, learning_ra
     # (x / 255 - 0.5) x 2
     # Get the output shape
     interm_shape = InceptionV3_notop.output_shape
-    global_pool = interm_shape[:2] if dim_ordering == 'tf' else interm_shape[1:]
+    global_pool = interm_shape[1:3] if dim_ordering == 'tf' else interm_shape[2:]
 
     # Change the input shape if we are testing
     if fcn and test:
@@ -30,8 +33,8 @@ def inception_model(input_shape=(256, 256, 3), fcn=True, test=False, learning_ra
     output = InceptionV3_notop.get_layer(index=-1).output  # Shape: (*, *, 2048)
     output = AveragePooling2D(global_pool, strides=global_pool, name='avg_pool')(output)  # Shape: (1, 1, 2048)
     if fcn:
-        activation = 'sigmoid' if test else 'softmax'
-        output = Convolution2D(8, 1, 1, activation=activation)(output)
+        # activation = 'sigmoid' if test else 'softmax'
+        output = Convolution2D(8, 1, 1, activation='sigmoid')(output)
         if not test:
             output = Flatten(name='flatten')(output)
     else:
@@ -46,3 +49,4 @@ def inception_model(input_shape=(256, 256, 3), fcn=True, test=False, learning_ra
     InceptionV3_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
     return InceptionV3_model
+

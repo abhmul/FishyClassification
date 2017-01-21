23869fb3136eb04ccf338d2817d817a8ec0f11a5
abhmul <abhmul@gmail.com>
2017-01-21 02:07:22 -0600

    Changed bounding box directory structure

--- src/transformations2.py
+++ src/transformations2.py
@@ -6,55 +6,7 @@ import unittest as tst
 import keras.backend as K
 
 
-def input_img(*args, **kwargs):
-    transform_mat = np.array([[1., 0., 0.],
-                              [0., 1., 0.],
-                              [0., 0., 1.]])
-    return lambda x, *args2, **kwargs2: (x, transform_mat)
-
-
-def random_rotation(rg, fill_mode='nearest', cval=0.):
-    # Take in a function, return a function that can be applied to x
-    def outer(func):
-        def inner(x, transform_mat=None, **kwargs):
-            return partial(rand_rot, rg=rg, fill_mode=fill_mode, cval=cval, **kwargs)(*func(x, transform_mat, **kwargs))
-        return inner
-    return outer
-    # return lambda func: \
-    #     (lambda x, channel_axis, row_axis, col_axis, transform_mat=None, **kwargs:
-    #      partial(rand_rot, rg=rg, fill_mode=fill_mode, cval=cval, channel_axis=channel_axis,
-    #              row_axis=row_axis, col_axis=col_axis)(func(x, transform_mat, **kwargs)))
-
-
-def random_shift(wrg, hrg, fill_mode='nearest', cval=0.):
-    # Take in a function, return a function that can be applied to x
-    return lambda func: \
-        (lambda x, transform_mat=None, **kwargs:
-         partial(rand_shift, wrg=wrg, hrg=hrg, fill_mode=fill_mode, cval=cval)(*func(x, transform_mat, **kwargs)))
-
-
-def random_shear(intensity, fill_mode='nearest', cval=0.):
-    # Take in a function, return a function that can be applied to x
-    return lambda func: \
-        (lambda x, transform_mat=None, channel_axis=0, row_axis=1, col_axis=2, **kwargs:
-         partial(rand_shear, intensity=intensity, fill_mode=fill_mode, cval=cval, **kwargs)(*func(x, transform_mat, **kwargs)))
-
-
-def random_zoom(zoom_range, fill_mode='nearest', cval=0.):
-    # Take in a function, return a function that can be applied to x
-    return lambda func: \
-        (lambda x, transform_mat=None, **kwargs:
-         partial(rand_zoom, zoom_range=zoom_range, fill_mode=fill_mode, cval=cval, **kwargs)(*func(x, transform_mat, **kwargs)))
-
-
-def random_channel_shift(intensity, fill_mode='nearest', cval=0.):
-    # Take in a function, return a function that can be applied to x
-    return lambda func: \
-        (lambda x, transform_mat=None, **kwargs:
-         partial(rand_channel_shift, intensity=intensity, fill_mode=fill_mode, cval=cval, **kwargs)(*func(x, transform_mat, **kwargs)))
-
-
-def compile_func(func, dim_ordering='default'):
+def compile_affine(funcs, dim_ordering='default', fill_mode='nearest', cval=0.):
     if dim_ordering == 'default':
         dim_ordering = K.image_dim_ordering()
     if dim_ordering not in {'tf', 'th'}:
@@ -69,147 +21,145 @@ def compile_func(func, dim_ordering='default'):
         channel_axis = 2
         row_axis = 0
         col_axis = 1
-    return partial(func, channel_axis=channel_axis, row_axis=row_axis, col_axis=col_axis)
+
+    def transform_img(x):
+        transform_matrix = None
+        h = x.shape[row_axis]
+        w = x.shape[col_axis]
+        for func in funcs:
+            transform_matrix = func(transform_matrix, h, w)
+        return apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
+
+    def transform_coord(coord, h, w):
+        transform_matrix = None
+        for func in funcs:
+            transform_matrix = func(transform_matrix, h, w)
+        vector = np.array([[coord[0]],
+                           [coord[1]],
+                           [1]])
+        transformed = np.dot(np.linalg.inv(transform_matrix), vector)
+        return transformed[0,0], transformed[1,0]
+
+    def transform_func(*args):
+        if len(args) == 3:
+            return transform_coord(*args)
+        elif len(args) == 1:
+            return transform_img(*args)
+        else:
+            raise ValueError('Input should be an ndimage or coordinate, height, width.')
+
+    return transform_func
 
 
-def rand_rot(x, transform_mat, rg, row_axis=1, col_axis=2, channel_axis=0,
-             fill_mode='nearest', cval=0., **kwargs):
-    """Performs a random rotation of a Numpy image tensor.
-
-    # Arguments
-        x: Input tensor. Must be 3D.
-        rg: Rotation range, in degrees.
-        row_axis: Index of axis for rows in the input tensor.
-        col_axis: Index of axis for columns in the input tensor.
-        channel_axis: Index of axis for channels in the input tensor.
-        fill_mode: Points outside the boundaries of the input
-            are filled according to the given mode
-            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
-        cval: Value used for points outside the boundaries
-            of the input if `mode='constant'`.
-
-    # Returns
-        Rotated Numpy image tensor.
-    """
-    theta = np.pi / 180 * np.random.uniform(-rg, rg)
-    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
-                                [np.sin(theta), np.cos(theta), 0],
+def apply_rotation(theta, h, w, prev_transform=None):
+    rad = np.pi / 180 * theta
+    rotation_matrix = np.array([[np.cos(rad), -np.sin(rad), 0],
+                                [np.sin(rad), np.cos(rad), 0],
                                 [0, 0, 1]])
-
-    h, w = x.shape[row_axis], x.shape[col_axis]
     transform_matrix = transform_matrix_offset_center(rotation_matrix, h, w)
-    transform_matrix_scale = transform_matrix_offset_center(rotation_matrix, 1., 1.)
-    x = apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
-    return x, np.dot(transform_matrix_scale, transform_mat)
+    if prev_transform is not None:
+        transform_matrix = np.dot(transform_matrix, prev_transform)
+    return transform_matrix
 
 
-def rand_shift(x, transform_mat, wrg, hrg, row_axis=1, col_axis=2, channel_axis=0,
-               fill_mode='nearest', cval=0., **kwargs):
-    """Performs a random spatial shift of a Numpy image tensor.
-
-    # Arguments
-        x: Input tensor. Must be 3D.
-        wrg: Width shift range, as a float fraction of the width.
-        hrg: Height shift range, as a float fraction of the height.
-        row_axis: Index of axis for rows in the input tensor.
-        col_axis: Index of axis for columns in the input tensor.
-        channel_axis: Index of axis for channels in the input tensor.
-        fill_mode: Points outside the boundaries of the input
-            are filled according to the given mode
-            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
-        cval: Value used for points outside the boundaries
-            of the input if `mode='constant'`.
-
-    # Returns
-        Shifted Numpy image tensor.
+def rotation(theta):
     """
-    h, w = x.shape[row_axis], x.shape[col_axis]
-    ty = int(np.random.uniform(-hrg, hrg)*h)
-    tx = int(np.random.uniform(-wrg, wrg)*w)
-    transform_matrix_scale = np.array([[1, 0, float(ty) / h],
-                                       [0, 1, float(tx) / w],
-                                       [0, 0, 1]])
+    Returns a function that takes transform, height, and width of a picture
+    adding on rotation transform for theta
+    """
+    def rot(transform, h, w):
+        return apply_rotation(theta, h, w, prev_transform=transform)
+
+    return rot
+
+
+def random_rotation(rg):
+    """
+    Returns a function that takes transform, height, and width of a picture
+    """
+
+    def rand_rot(transform, h, w):
+        return apply_rotation(np.random.uniform(-rg, rg), h, w, prev_transform=transform)
+
+    return rand_rot
+
+
+def shift(tx, ty, h=None, w=None, prev_transform=None):
+    #TODO figure out which shift changes which coordinate
     translation_matrix = np.array([[1, 0, ty],
                                    [0, 1, tx],
                                    [0, 0, 1]])
 
     transform_matrix = translation_matrix  # no need to do offset
-    x = apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
-    return x, np.dot(transform_matrix_scale, transform_mat)
+    if prev_transform is not None:
+        transform_matrix = np.dot(transform_matrix, prev_transform)
+    return transform_matrix
 
 
-def rand_shear(x, transform_mat, intensity, row_axis=1, col_axis=2, channel_axis=0,
-                 fill_mode='nearest', cval=0., **kwargs):
-    """Performs a random spatial shear of a Numpy image tensor.
-
-    # Arguments
-        x: Input tensor. Must be 3D.
-        intensity: Transformation intensity.
-        row_axis: Index of axis for rows in the input tensor.
-        col_axis: Index of axis for columns in the input tensor.
-        channel_axis: Index of axis for channels in the input tensor.
-        fill_mode: Points outside the boundaries of the input
-            are filled according to the given mode
-            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
-        cval: Value used for points outside the boundaries
-            of the input if `mode='constant'`.
-
-    # Returns
-        Sheared Numpy image tensor.
+def random_shift(wrg, hrg):
     """
-    shear = np.random.uniform(-intensity, intensity)
-    shear_matrix = np.array([[1, -np.sin(shear), 0],
-                             [0, np.cos(shear), 0],
+    Returns a function that takes transform, height, and width of a picture
+    and applies a random shift to it
+    """
+    def rand_shift(transform, h, w):
+        ty = int(np.random.uniform(-hrg, hrg)*h)
+        tx = int(np.random.uniform(-wrg, wrg)*w)
+        return shift(tx, ty, prev_transform=transform)
+
+    return rand_shift
+
+
+def shear(shear_val, h, w, prev_transform=None):
+    shear_matrix = np.array([[1, -np.sin(shear_val), 0],
+                             [0, np.cos(shear_val), 0],
                              [0, 0, 1]])
-
-    h, w = x.shape[row_axis], x.shape[col_axis]
     transform_matrix = transform_matrix_offset_center(shear_matrix, h, w)
-    transform_matrix_scale = transform_matrix_offset_center(shear_matrix, 1., 1.)
-    x = apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
-    return x, np.dot(transform_matrix_scale, transform_mat)
+    if prev_transform is not None:
+        transform_matrix = np.dot(transform_matrix, prev_transform)
+    return transform_matrix
 
 
-def rand_zoom(x, transform_mat, zoom_range, row_axis=1, col_axis=2, channel_axis=0,
-                fill_mode='nearest', cval=0., **kwargs):
-    """Performs a random spatial zoom of a Numpy image tensor.
+def random_shear(intensity):
+    """
+    Returns a function that takes transform, height, and width of a picture
+    and applies a random shear to it
+    """
+    def rand_shear(transform, h, w):
+        return shear(np.random.uniform(-intensity, intensity), h, w, prev_transform=transform)
+    return rand_shear
 
-    # Arguments
-        x: Input tensor. Must be 3D.
-        zoom_range: Tuple of floats; zoom range for width and height.
-        row_axis: Index of axis for rows in the input tensor.
-        col_axis: Index of axis for columns in the input tensor.
-        channel_axis: Index of axis for channels in the input tensor.
-        fill_mode: Points outside the boundaries of the input
-            are filled according to the given mode
-            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
-        cval: Value used for points outside the boundaries
-            of the input if `mode='constant'`.
 
-    # Returns
-        Zoomed Numpy image tensor.
+def zoom(zx, zy, h, w, prev_transform=None):
+    # TODO see which order zx and zy should be in
+    zoom_matrix = np.array([[zx, 0, 0],
+                            [0, zy, 0],
+                            [0, 0, 1]])
 
-    # Raises
-        ValueError: if `zoom_range` isn't a tuple.
+    transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
+    if prev_transform is not None:
+        transform_matrix = np.dot(transform_matrix, prev_transform)
+    return transform_matrix
+
+
+def random_zoom(zoom_range):
+    """
+    Returns a function that takes transform, height, and width of a picture
+    and applies a random zoom to it
     """
     if len(zoom_range) != 2:
         raise ValueError('zoom_range should be a tuple or list of two floats. '
                          'Received arg: ', zoom_range)
 
-    if zoom_range[0] == 1 and zoom_range[1] == 1:
-        zx, zy = 1, 1
-    else:
-        zx, zy = np.random.uniform(zoom_range[0], zoom_range[1], 2)
-    zoom_matrix = np.array([[zx, 0, 0],
-                            [0, zy, 0],
-                            [0, 0, 1]])
-
-    h, w = x.shape[row_axis], x.shape[col_axis]
-    transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
-    transform_matrix_scale = transform_matrix_offset_center(zoom_matrix, 1., 1.)
-    x = apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
-    return x, np.dot(transform_matrix_scale, transform_mat)
+    def rand_zoom(transform, h, w):
+        if zoom_range[0] == 1 and zoom_range[1] == 1:
+            zx, zy = 1, 1
+        else:
+            zx, zy = np.random.uniform(zoom_range[0], zoom_range[1], 2)
+        return zoom(zx, zy, h, w, prev_transform=transform)
+    return rand_zoom
 
 
+# Not an Affine Transform
 def rand_channel_shift(x, transform_mat, intensity, channel_axis=0, **kwargs):
     x = np.rollaxis(x, channel_axis, 0)
     min_x, max_x = np.min(x), np.max(x)
@@ -243,28 +193,37 @@ def apply_transform(x, transform_matrix, channel_axis=0, fill_mode='nearest', cv
 class TestTransforms(tst.TestCase):
 
     def testRotate(self):
+        transformer = compile_affine([rotation(45)])
+
         # Check it doesn't change the shape
-        img = input_img()
-        img = random_rotation(20.)(img)
-        transformer = compile_func(img)
-
-        test_arr = np.linspace(0.0, 1.0, 500 * 1000).reshape((500, 1000, 1))
-        print transformer(test_arr)[1]
-        self.assertEqual(transformer(test_arr)[0].shape, test_arr.shape)
-
-    def testShift(self):
-        # Check it doesn't change the shape
-        img = input_img()
-        img = random_rotation(20.)(img)
-        img = random_shift(.1, .1)(img)
-        img = random_shear(.1)(img)
-        img = random_zoom((.8, 1.2))(img)
-        img = random_channel_shift(.1)(img)
-        transformer = compile_func(img)
-
-        test_arr = np.linspace(0.0, 1.0, 500 * 1000).reshape((500, 1000, 1))
-        print transformer(test_arr)[1]
-        self.assertEqual(transformer(test_arr)[0].shape, test_arr.shape)
+        shape = (100, 200)
+        loc = (int(shape[0] * .7), int(shape[1] * .4))
+        test_arr = np.zeros(shape + (1,))
+        test_arr[loc] = 1
+        out = transformer(test_arr).reshape(shape)
+        print test_arr.reshape(shape) - out
+        print loc
+        print transformer(loc, *shape)
+        rounded_coords = tuple(int(round(i)) for i in (transformer(loc, *shape)))
+        print rounded_coords
+        self.assertEqual(out.shape, shape)
+        print out[rounded_coords]
+        print np.where(out==1.)
+        self.assertEqual(out[rounded_coords], 1.)
+    #
+    # def testShift(self):
+    #     # Check it doesn't change the shape
+    #     img = input_img()
+    #     img = random_rotation(20.)(img)
+    #     img = random_shift(.1, .1)(img)
+    #     img = random_shear(.1)(img)
+    #     img = random_zoom((.8, 1.2))(img)
+    #     img = random_channel_shift(.1)(img)
+    #     transformer = compile_func(img)
+    #
+    #     test_arr = np.linspace(0.0, 1.0, 500 * 1000).reshape((500, 1000, 1))
+    #     print transformer(test_arr)[1]
+    #     self.assertEqual(transformer(test_arr)[0].shape, test_arr.shape)
 
 if __name__ == '__main__':
     tst.main()
